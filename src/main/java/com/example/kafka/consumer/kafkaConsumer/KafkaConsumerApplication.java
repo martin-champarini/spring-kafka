69c917364f5aeadc34b7446a120f746a07c1ccef
Unknown <martin.champarini.dev@gmail.com>
2018-07-26 21:45:14 -0300

    First commit

---
+++ src/main/java/com/example/kafka/consumer/kafkaConsumer/KafkaConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.example.kafka.consumer.kafkaConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerApplication.class, args);
+	}
+}
